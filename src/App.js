4762290c6f7ac837f99e71d00fe7bfaf0c0c3206
zeroesandones01 <z3r0sh1k10707@gmail.com>
2024-01-04 23:35:52 +0800

    Commit sa bahay

--- src/App.js
+++ src/App.js
@@ -4,19 +4,67 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+//CLASS COMPONENT //OLD
+// class App extends Component{
+//   constructor(){
+//     super();
+//     this.state = {
+//       input: '',
+//     }
+//   }
+
+//   onInputChange = (event) => {
+//     console.log(event.target.value);
+//   }
+
+
+//   render() {
+//     return (
+    
+//       <div className="App">
+//         <ParticlesBg type="cobweb" bg={true} />
+//         <Navigation />
+//         <Logo />
+//         <Rank />
+//         <ImageLinkForm onInputChange={this.onInputChange}/>
+//         {/*
+        
+//         <FaceRecognition />} */}
+//       </div>
+//     )
+//   }
+// };
+
+
+
+//functional component
+const App = () => {
+  //const [input, setInput] = useState('');
+  const [setInput] = useState('');
+
+  const onInputChange = (event) => {
+    console.log(event.target.value);
+    setInput(event.target.value);
+  }
+
+  const onButtonSubmit = () =>{
+    console.log('clickk');
+  }
+
   return (
     <div className="App">
       <ParticlesBg type="cobweb" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm 
+      onInputChange={onInputChange} 
+      onButtonSubmit={onButtonSubmit}/>
       {/*
-      
-      <FaceRecognition />} */}
+      <FaceRecognition /> */}
     </div>
   );
 }
