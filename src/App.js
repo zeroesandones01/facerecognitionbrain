9e5b4a061f071dea1cc9c339e4097017dd4952b2
zeroesandones01 <z3r0sh1k10707@gmail.com>
2024-01-04 17:17:49 +0800

    Commit sa work

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ParticlesBg type="cobweb" bg={true} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/*
+      
+      <FaceRecognition />} */}
+    </div>
+  );
+}
+
+export default App;
